14b3d7ae90310bb839e38a65d6683047e6d5a849
yht0827 <yangheetae0827@naver.com>
2024-12-23 20:40:31 +0900

    Initial commit

---
+++ src/main/java/com/example/hhpluslecture/HhplusLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.hhpluslecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusLectureApplication.class, args);
+	}
+
+}
